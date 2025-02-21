6d92c08878d674da65f14c5f9cb73cebfb99e387
Reham Mohamed <166271252+rehamfoaud@users.noreply.github.com>
2025-02-21 07:13:40 +0200

    Add files via upload

---
+++ important info about problem/methods.js
@@ -0,0 +1,18 @@
+//1-> toFixed(number)=>   بتقرب لاقرب رقم number
+//Example:
+//4.56656.toFixed(2) => 4.57
+/////////////////////////////////////////////////////
+
+
+//2->charCodeAt()=> return index of letter by Unicode.
+let word="hello"
+document.write(word.charCodeAt("h")) // 104
+// A->65 b->66 c->67 d->68
+// a=97 b=98 c=99 d=100
+
+////////////////////////////////////////
+ //3-> indexOf() -> return index of letter in word
+/********************************************************* */
+//4-> replace () with string only
+
+
